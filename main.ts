9b049618a7dc411564dc62e1393d05ff19d5ff92
Sayed Rafi <sayedarrafi.bd@gmail.com>
2024-10-12 03:40:57 +0900

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,66 @@
+// variable declaration
+// let var1 = 1;
+// var1 = 2;
+// console.log(var1)
+
+
+// constant declaration
+// const val1 = 2;
+// console.log(val1)
+
+// basic types
+// const val1:number = 1;
+// const val2:string = "1";
+// const val3:boolean = false;
+
+// array
+// const num_arr:number[] = [1,2,3,4]
+// num_arr.push(5)
+
+// const copy_num_arr = [0, ...num_arr, 6]
+
+// console.log(copy_num_arr)
+
+// object
+// const person1 = {
+//     name: "Rafi",
+//     age: 18,
+//     class: 12,
+//     married: true
+// }
+// person1.age = 28
+// person1["name"] = "Turzo"
+
+// const person2 = {...person1, name: "Masud"}
+
+// console.log(person2)
+
+// loop
+// const arr = [1,2,3,4,5,6]
+// arr.forEach((e, i) => {
+//     console.log(e, i)
+// })
+
+// map
+
+// filter
+
+// function (does something)
+// function doSummation(value1:number, value2:number){
+//     console.log(value1 + value2)
+// }
+
+// doSummation(1, 2)
+// doSummation(3,4)
+
+// function (return something)
+// function getSummation(value1:number, value2:number){
+//     return (value1 + value2)
+// }
+// const sum = getSummation(1,2)
+// console.log(sum)
+
+// concat
+// const a = "My Name "
+// console.log(a + "is Rafi")
+// console.log(`${a} is Rafi`)
