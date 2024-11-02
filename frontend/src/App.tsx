b397245f9a24238f11d313ba483af432b77a0f39
Sayed Rafi <sayedarrafi.bd@gmail.com>
2024-11-02 23:00:37 +0900

    up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import SignIn from "./pages/SignIn";
+import ClassDetails from "./pages/ClassDetails";
+import ClassScheduler from "./pages/ClassScheduler";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/class/details",
+    element: <ClassDetails />,
+  },
+  {
+    path: "/class/scheduler",
+    element: <ClassScheduler />,
+  },
+]);
 
 function App() {
   return (
-    <div className='p-5 bg-gray-100 h-screen w-full flex items-center justify-center'>
-      <div className="rounded-lg border border-gray-300 p-5 w-96 bg-white shadow">
-          <div className="text-2xl font-semibold text-center">Welcome Back 😊</div>
-          <div className="opacity-75 text-center">We have missed you so much !!</div>
-          <div className="mt-8 flex flex-col gap-5">
-            <input type="text" className='input input-bordered' placeholder='Email address' />
-            <input type="text" className='input input-bordered' placeholder='Password' />
-            <button className="btn btn-accent">Sign In</button>
-          </div>
-      </div>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ frontend/src/pages/ClassScheduler.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ClassScheduler() {
+  return (
+    <div>ClassScheduler</div>
+  )
+}
+
+export default ClassScheduler
